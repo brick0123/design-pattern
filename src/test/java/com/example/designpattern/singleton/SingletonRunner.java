b2b33c8decf5fe753879872afad50c0ff5bd3a79
brick0123 <tlfanfvk@gmail.com>
2022-10-29 16:51:33 +0900

    feat: 싱글턴 리플렉션으로  파괴

--- src/test/java/com/example/designpattern/singleton/SingletonRunner.java
+++ src/test/java/com/example/designpattern/singleton/SingletonRunner.java
@@ -1,6 +1,9 @@
 package com.example.designpattern.singleton;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,5 +13,16 @@ public class SingletonRunner {
     void v1() {
         final SingletonV1 instance = SingletonV1.getInstance();
         assertThat(instance).isSameAs(SingletonV1.getInstance());
+    }
+
+    @Test
+    @DisplayName("리플렉션을 이용해서 싱글턴을 파괴함")
+    void singletone_break() throws Exception {
+        final Constructor<SingletonV5> constructor = SingletonV5.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+
+        final SingletonV5 instance = constructor.newInstance();
+
+        assertThat(instance).isNotSameAs(SingletonV5.getInstance());
     }
 }
---
+++ src/test/java/com/example/designpattern/singleton/SingletonV6.java
@@ -0,0 +1,9 @@
+package com.example.designpattern.singleton;
+
+/**
+ * 리플렉션에 안전함
+ */
+public enum SingletonV6 {
+
+    INSTANCE;
+}
